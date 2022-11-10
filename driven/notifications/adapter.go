0e4341cd6daa9c4bceb62aa1a76a944f790d7190
Stephen Hurwit <sjhurwit@gmail.com>
2022-11-10 10:58:34 -0600

    fix lint issues

--- driven/notifications/adapter.go
+++ driven/notifications/adapter.go
@@ -64,13 +64,13 @@ func (a *Adapter) SendNotification(recipients []model.NotificationRecipient, top
 }
 
 // SendMail sends email to a user
-func (na *Adapter) SendMail(toEmail string, subject string, body string) {
-	go na.sendMail(toEmail, subject, body)
+func (a *Adapter) SendMail(toEmail string, subject string, body string) {
+	go a.sendMail(toEmail, subject, body)
 }
 
-func (na *Adapter) sendMail(toEmail string, subject string, body string) error {
+func (a *Adapter) sendMail(toEmail string, subject string, body string) error {
 	if len(toEmail) > 0 && len(subject) > 0 && len(body) > 0 {
-		url := fmt.Sprintf("%s/api/int/mail", na.baseURL)
+		url := fmt.Sprintf("%s/api/int/mail", a.baseURL)
 
 		bodyData := map[string]interface{}{
 			"to_mail": toEmail,
@@ -89,7 +89,7 @@ func (na *Adapter) sendMail(toEmail string, subject string, body string) error {
 			log.Printf("error creating load user data request - %s", err)
 			return err
 		}
-		req.Header.Set("INTERNAL-API-KEY", na.internalAPIKey)
+		req.Header.Set("INTERNAL-API-KEY", a.internalAPIKey)
 
 		resp, err := client.Do(req)
 		if err != nil {
