8373d46bc5631b5d82c1ef79be182a2f83b83c6c
Mladen Dryankov <mdryankov@gmail.com>
2022-11-18 21:32:23 +0200

    [#30] Send notifications for group polls directly through groups bb (#31)
    * Fix build (used the groups bb Makefile)
    
    * Send notifications for group polls directly through Groups BB
    
    * Standardise message structure along the bbs
    
    * Fix missing pieces. Partial redo of removed logic (send notification directly through notifications BB for non group polls).
    
    * Redo
    
    Co-authored-by: Stephen Hurwit <10472810+shurwit@users.noreply.github.com>

--- core/model/user.go
+++ core/model/user.go
@@ -7,3 +7,15 @@ type User struct {
 	Token  string
 	Claims tokenauth.Claims
 }
+
+// UserRef reference for a concrete user which is member of a group
+type UserRef struct {
+	UserID string `json:"user_id"`
+	Name   string `json:"name"`
+} // @name MemberRecipient
+
+// Sender Wraps sender type and user ref
+type Sender struct {
+	Type string   `json:"type" bson:"type"` // user or system
+	User *UserRef `json:"user,omitempty" bson:"user,omitempty"`
+} // @name Sender
